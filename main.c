9b88102a3b2d81dace63ad48c94ad872772e2eca
Axel Ferreira <axelferreira@me.com>
2013-04-08 10:26:47 +0100

    Adding PATH_MODE for test purpose in Xcode

--- main.c
+++ main.c
@@ -21,13 +21,15 @@
 #define INIT_BUFFER_SIZE 500
 #define FILE_NAME_BUFFER 50
 
-#define PATH_MODE               // <------------------------<<<<<<<     Uncomment/Comment to switch PATH_MODE
-#ifdef PATH_MODE
+#define DEBUG_MODE 1                    // <-----<<<  Toogle 1/0 to switch DEBUG_MODE ON/OFF
+#define PATH_MODE TRUE                      // <-----<<<  Comment to switch PATH_MODE OFF
+#ifdef PATH_MODE 
     #define L_FILE "/Users/axelferreira/Desktop/dir/lista.txt"
     #define D_FILE "/Users/axelferreira/Desktop/dir/D.txt"
     #define E_FILE "/Users/axelferreira/Desktop/dir/E.txt"
     #define G_FILE "/Users/axelferreira/Desktop/dir/G.txt"
-#else 
+    #define _PATH_ "/Users/axelferreira/Desktop/dir/"
+#else
     #define L_FILE "lista.txt"
     #define D_FILE "D.txt"
     #define E_FILE "E.txt"
@@ -42,20 +44,29 @@ static int nCon = 0;
 int leFicheiro(char * bufferList)
 {   int indexC_J = -1, ret=TRUE, lRej=0, bLine_size = (INIT_BUFFER_SIZE * sizeof(char));
     char * bufferLine=NULL;
-    indexC_J = openFile(bufferList, "r");
-    FILE * f2 = getFile(indexC_J);          // apontador para o ficheiro
-    bufferLine = (char *) malloc(INIT_BUFFER_SIZE * sizeof(char));
-    // ^ Abre o ficheiro no controlo && cria um buffer.
+    char * s = malloc(100 * sizeof(char));
     
+    if (PATH_MODE)
+    {   strcpy(s, _PATH_);
+        strcpy(&s[32], bufferList);
+        indexC_J = openFile(s, "r");
+    }
+    else {indexC_J = openFile(bufferList, "r");}
+    
+    bufferLine = (char *) realloc(NULL, INIT_BUFFER_SIZE * sizeof(char));
+    // ^ Abre o ficheiro no controlo && cria um buffer.
+
     // Caso tenha conseguido abrir o ficheiro e alocar o buffer
     if (indexC_J!=-1 && bufferLine)
     {   char type = bufferList[0];
         // Le cada linha do ficheiro
-        while (!feof(f2))
-        {   ret=dynamic_read_line(&bufferLine, &bLine_size, indexC_J);
+        while (ret) //   <---------------------------####### PROBLEM HERE
+        { Stats entrada;
+            ret=dynamic_read_line(&bufferLine, &bLine_size, indexC_J);
             if (ret==-1) {return ret;}
-            Stats entrada;
-            
+            if (DEBUG_MODE) {printf("Buffer:#%s#\n",bufferLine);}
+
+
             // Valida a linha
             entrada = parseLine(bufferLine, type);
             if (entrada.nomes)      
@@ -85,8 +96,9 @@ int main(int argc, const char * argv[])
 {   int i, bool = TRUE, indexL=-1;
     char * bufferList;
 	int buffer_size = FILE_NAME_BUFFER * sizeof(char);
-	int rc;
+	int rc = TRUE;
     
+
     // Inicializa o controlo & Abre o ficheiro lista.txt && Alloca o buffer
     init_file_control();                        // Inicializa o controlo de ficheiros
     indexL = openFile(L_FILE, "r");             // Abre o ficheiro lista.txt
@@ -97,29 +109,32 @@ int main(int argc, const char * argv[])
     rc = dynamic_read_line(&bufferList, &buffer_size, indexL);
     i = atoi(bufferList);                       // Converte num inteiro
     setMinPag(i);                               // Guarda o numero minimo de paginas.
-    
+
     
     // Ciclo que percorre todo o ficheiro  lista.txt
 	while(rc)
-	{   // Pede uma linha do ficheiro lista.txt atraves do modulo fread
+	{ int lRej=0;
+
+        // Pede uma linha do ficheiro lista.txt atraves do modulo fread
         rc = dynamic_read_line(&bufferList, &buffer_size, indexL);
-        if (rc == -1) { return rc; }            // caso o ficheiro tenha acabado para
-        int lRej=0;
-        // Funcao que le o ficheiro e retorna o numero de rejeritados nesse ficheiro
-        lRej = leFicheiro(bufferList);
-        // Imprime o nome do ficheiro e o numero de Rejeitados.
-        imprimeE(bool,lRej,bufferList);
-        bool = FALSE;
+
+        if (rc)         // caso o ficheiro não tenha acabado para
+        {
+            // REMOVE OS \n do BUFFER
+            for (i=0;i<buffer_size && bufferList[i]!='\n'; i++) {}
+            if (bufferList[i]=='\n') {bufferList[i]='\0';}
+            
+            // Funcao que le o ficheiro e retorna o numero de rejeitados nesse ficheiro
+            lRej = leFicheiro(bufferList);
+            // Imprime o nome do ficheiro e o numero de Rejeitados.
+            imprimeE(bool,lRej,bufferList, E_FILE);
+            bool = FALSE;
+        }
     }
 	free(bufferList);
     // Imprime o ficheiro D
-    imprimeD(nRej, nJou, nCon);
-    
-    
-    
-    
-    
-    
+    imprimeD(nRej, nJou, nCon, D_FILE);
+
     
     closeFile(indexL);
     return 0;
@@ -174,6 +189,10 @@ int main(int argc, const char * argv[])
     
     }
     
+     
+     
+     
+     
 if (DEBUG_MODE==TRUE)
 {
     time_t t = time(NULL);
